6ad3163eb2e014180252c3b7faf57002cc670832
Brandon Nichols <brandonaronnichols@gamil.com>
2020-05-28 23:44:35 -0500

    refactored binary tree part 1 to be recursive

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -24,37 +24,17 @@ class BSTNode:
     # Insert the given value into the tree
     def insert(self, value):
         new_branch = BSTNode(value)
-        current_node = None
-        prev_node = None
-        greater_than_prev_node = True
-
+        
         if value >= self.value:
-            if self.right is None:
+            if self.right:
+                self.right.insert(value)
+            else:
                 self.right = new_branch
-                return
-            else:
-                current_node = self.right
         else:
-            if self.left is None:
+            if self.left:
+                self.left.insert(value)
+            else:
                 self.left = new_branch
-                return
-            else:
-                current_node = self.left
-
-        while current_node:
-            if value >= current_node.value:
-                prev_node = current_node
-                current_node = current_node.right
-                greater_than_prev_node = True
-            else:
-                prev_node = current_node
-                current_node = current_node.left
-                greater_than_prev_node = False
-
-        if greater_than_prev_node:
-            prev_node.right = new_branch
-        else:
-            prev_node.left = new_branch
         # new_branch = BSTNode(value)
         # current_node = None
         # can_insert = False
@@ -94,43 +74,29 @@ class BSTNode:
 
     # Return True if the tree contains the value
     # False if it does not
+    
     def contains(self, target):
-        contains_value = False
-        current_node = None
-
         if target == self.value:
-            contains_value = True
-            return contains_value
+            return True
+
+        if target > self.value:
+            if self.right:
+                return self.right.contains(target)
         else:
-            if target > self.value:
-                current_node = self.right
-            else:
-                current_node = self.left
-
-            while current_node:
-                if current_node.value == target:
-                    contains_value = True
-                    return contains_value
-
-                if current_node.value > target:
-                    current_node = current_node.right
-                else:
-                    current_node = current_node.left
+            if self.left:
+                return self.left.contains(target)
+        
+        return False
+        
 
 
     # Return the maximum value found in the tree
-    def get_max(self):
-        current_max = self.value
-        current_node = None
 
+    def get_max(self):
         if self.right is None:
-            return current_max
+            return self.value
         else:
-            current_node = self.right
-            while current_node:
-                current_max = current_node.value
-                current_node = current_node.right
-            return current_max
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
